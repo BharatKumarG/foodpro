9a58af35c1cc121b4e2971683aa9cd1591481812
Guduru Bharat Kumar <bharathkumarguduru443@gmail.com>
2025-07-24 16:43:31 +0530

    changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import FeaturedDishes from '../components/FeaturedDishes';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedDishes />
+      <div className="text-center mt-8">
+        <Link
+          to="/menu"
+          className="bg-orange-500 text-white px-6 py-3 rounded-xl font-medium hover:bg-orange-600 transition-all duration-200"
+        >
+          View Full Menu
+        </Link>
+      </div>
     </div>
   );
 };
